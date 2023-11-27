60efe6d789dd18c498811619db2e98f175602410
Alvaro <alvarosanchezvelez1@gmail.com>
2023-11-27 11:52:49 +0100

    delete useless file

--- src/app/components/clientes/clientes.json.ts
+++
@@ -1,9 +0,0 @@
-import { Cliente } from './cliente';
-
-export const CLIENTES: Cliente[] = [
-    {id: 1, nombre: 'Álvaro', apellido: 'Sánchez',email: 'alvarosanchezvelez1@gmail.com', createAt: '16/11/2023', },
-    {id: 2, nombre: 'Alberto', apellido: 'Rodríguez',email: 'albertordgz@gmail.com', createAt: '16/11/2023', },
-    {id: 3, nombre: 'María', apellido: 'Nuñez',email: 'marianñz@gmail.com', createAt: '16/11/2023', },
-    {id: 4, nombre: 'Jaime', apellido: 'Pérez',email: 'jaimeprz@gmail.com', createAt: '16/11/2023', },
-    {id: 5, nombre: 'Laura', apellido: 'Castaño',email: 'lauractñ@gmail.com', createAt: '16/11/2023', }
-  ];
